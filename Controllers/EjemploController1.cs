4e34b31dd3e27872a49e2adcc4b595ca234d98cd
CRIST <CRIST@LENOVO>
2023-10-12 09:56:01 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/EjemploController1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaginaWebCSharp.Models;
+
+namespace PaginaWebCSharp.Controllers
+{
+    public class EjemploController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contacto()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Christian";
+            persona.Apellidos = "Cauich";
+            persona.FechaNacimiento = new DateTime(2003, 09, 15);
+
+            return View(persona);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaginaWebCSharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
